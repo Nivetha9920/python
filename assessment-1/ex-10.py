6ef516263ed5d86d65dd449f489b053037136d6a
Nivetha9920 <97342277+Nivetha9920@users.noreply.github.com>
2022-01-17 19:33:00 +0530

    Delete ex-10.py

--- assessment-1/ex-10.py
+++
@@ -1,8 +0,0 @@
-dict = {'key1':'python', 'key2':'is'}
-print("Current Dict is: ", dict)
-  
-dict1 = {'key3':'a', 'key4':'programming', 'key5':'language'}
-dict.update(dict1)
-
-dict.update(newkey1 ='portal')
-print(dict)
