6bf66a6b6eaf3492e98dcaf0451f16a2800edb20
Nivetha9920 <97342277+Nivetha9920@users.noreply.github.com>
2022-01-31 13:11:30 +0530

    Update ex-2.py

--- assessment-2/ex-2.py
+++ assessment-2/ex-2.py
@@ -1,18 +1,14 @@
 # import library
 import re
-
 # url link
 s =input("Enter the url:")
 print("URL:",s)
 # finding the hostname which may
 # contain dash or dots
-obj2 = re.findall('://www.([\w\-\.]+)',
-				s)
+obj2 = re.findall('://www.([\w\-\.]+)',s)
 print("Host:",obj2)
-
 # finding the protocol
-obj1 = re.findall('(\w+)://',
-				s)
+obj1 = re.findall('(\w+)://',s)
 print("Protocol:",obj1)
 # finding the port number
 obj3 = re.findall(':(\d+)', s)
