fd0e8113700df0590a96fca1cea151b5f444270f
Nivetha9920 <97342277+Nivetha9920@users.noreply.github.com>
2022-02-22 20:58:42 +0530

    Create ex2.py

---
+++ Assessment 4/ex2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df1=pd.DataFrame({
+    'Name':['Jai', 'Prince', 'Gaurav', 'Anuj'],
+	'Age':[27, 24, 22, 32],
+	'Qualification':['Msc', 'MA', 'MCA', 'Phd']
+})
+df1['Address'] = ['Delhi', 'Kanpur', 'Allahabad', 'Mumbai']
+df1
